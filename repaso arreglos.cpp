389c32b7767d0b2250653071f4ba952146be4c3d
Al3x666 <christopher.gutierrez35@my.unitec.edu.mx>
2024-10-28 15:40:53 -0600

    Create repaso arreglos.cpp

---
+++ repaso arreglos.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Declaración y inicialización de un arreglo de enteros con 5 elementos
+    int numeros[5] = { 10, 20, 30, 40, 50 };
+
+    // Mostrar los elementos del arreglo
+    cout << "Elementos en el arreglo: ";
+    for (int i = 0; i < 5; i++) {
+        cout << numeros[i] << " ";
+    }
+    cout << endl;
+
+    // Modificar un elemento del arreglo
+    numeros[2] = 35;
+
+    // Mostrar el arreglo actualizado
+    cout << "Arreglo después de modificar el tercer elemento: ";
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << numeros[i] << " ";
+    }
+    cout << endl;
+
+    // Calcular la suma de los elementos en el arreglo
+    int suma = 0;
+    for (int i = 0; i < 5; i++) 
+    {
+        suma += numeros[i];
+    }
+    cout << "La suma de los elementos es: " << suma << endl;
+}
+
